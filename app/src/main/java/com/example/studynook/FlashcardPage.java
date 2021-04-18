a4bdad4467bb167dcfe67fceb8904cfb263164fd
JenniferNou <jennifer.hieng@gmail.com>
2021-04-19 10:31:42 +1200

    Main pages

--- app/src/main/java/com/example/studynook/FlashcardPage.java
+++ app/src/main/java/com/example/studynook/FlashcardPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FlashcardPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flashcard_page);
     }
 }
