2d32a00b77d12d6e33291e85efb8d55511d0b7f2
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2021-04-26 15:12:53 +1200

    Note Page done

--- app/src/main/java/com/example/studynook/CreatePage.java
+++ app/src/main/java/com/example/studynook/CreatePage.java
@@ -1,15 +1,22 @@
 package com.example.studynook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class CreatePage extends AppCompatActivity {
 
-    private Button notiifications, profile, timer, resources, home, wellBeing, notes;
+    private Button note, flashcard;
+    private ImageButton notiifications, profile, planner, resources, home, wellBeing, create;
     private BottomAppBar bottom;
 
     @Override
@@ -17,13 +24,111 @@ public class CreatePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_page);
 
-        notiifications = findViewById(R.id.notifications);
-        profile = findViewById(R.id.profile);
-        timer = findViewById(R.id.clock);
-        resources = findViewById(R.id.resources);
+        planner = findViewById(R.id.plannerButton);
+        resources = findViewById(R.id.resourcesButton);
         home = findViewById(R.id.homeButton);
-        wellBeing = findViewById(R.id.wellBeing);
-        notes = findViewById(R.id.notes);
+        wellBeing = findViewById(R.id.wellBeingButton);
+        create = findViewById(R.id.createButton);
         bottom = findViewById(R.id.bottomAppBar5);
+        note = findViewById(R.id.noteButton);
+        flashcard = findViewById(R.id.flashcardButton);
+
+        planner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePage.this, PlannerPage.class);
+                startActivity(intent);
+            }
+        });
+
+        resources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePage.this, ResourcesPage.class);
+                startActivity(intent);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        wellBeing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePage.this, WellbeingPage.class);
+                startActivity(intent);
+            }
+        });
+
+        /*create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePage.this, CreatePage.class);
+                startActivity(intent);
+            }
+        });*/
+
+        note.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePage.this, NotePage.class);
+                startActivity(intent);
+            }
+        });
+
+        flashcard.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreatePage.this, FlashcardPage.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_top_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.notibutton:
+                //startActivity(new Intent(this, ));
+                overridePendingTransition(0, 0);
+                return true;
+
+            case R.id.profilebutton:
+                startActivity(new Intent(getApplicationContext(), ProfilePage.class));
+                overridePendingTransition(0, 0);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //@Override
+    /*public boolean onNavigationItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.notibutton:
+                //startActivity(new Intent(this, ));
+                overridePendingTransition(0, 0);
+                return true;
+
+            case R.id.profilebutton:
+                startActivity(new Intent(getApplicationContext(), ProfilePage.class));
+                overridePendingTransition(0, 0);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
