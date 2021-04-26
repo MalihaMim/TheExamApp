b13f90fd76fee13f6a2c1ac5ea97f6416e9c37b1
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2021-04-26 16:12:14 +1200

    Note Page done

--- app/src/main/java/com/example/studynook/PlannerPage.java
+++ app/src/main/java/com/example/studynook/PlannerPage.java
@@ -1,8 +1,15 @@
 package com.example.studynook;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PlannerPage extends AppCompatActivity {
 
