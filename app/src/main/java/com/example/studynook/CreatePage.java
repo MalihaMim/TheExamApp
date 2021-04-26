af0e99a578398c70bc7bcdf16fa62081e27db2a8
PhongNguyen13 <81122421+PhongNguyen13@users.noreply.github.com>
2021-04-27 02:07:22 +1200

    Action Bar done

--- app/src/main/java/com/example/studynook/CreatePage.java
+++ app/src/main/java/com/example/studynook/CreatePage.java
@@ -1,11 +1,16 @@
 package com.example.studynook;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +20,11 @@ public class CreatePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_page);
+
+        ActionBar bar = getSupportActionBar();
+        ColorDrawable color = new ColorDrawable(Color.parseColor("#D0F2CA"));
+        bar.setBackgroundDrawable(color);
+        bar.setTitle("Create");
 
         // Initialise and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -44,10 +54,33 @@ public class CreatePage extends AppCompatActivity {
                         overridePendingTransition(0, 0);
                         return true;
                     case R.id.create:
-                        return true;
+                      return true;
                 }
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_top_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.notibutton:
+                //startActivity(new Intent(this, ));
+                overridePendingTransition(0, 0);
+                return true;
+
+            case R.id.profilebutton:
+                startActivity(new Intent(getApplicationContext(), ProfilePage.class));
+                overridePendingTransition(0, 0);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/studynook/ResourcesPage.java
+++ app/src/main/java/com/example/studynook/ResourcesPage.java
@@ -1,10 +1,14 @@
 package com.example.studynook;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,6 +19,11 @@ public class ResourcesPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources_page);
+
+        ActionBar bar = getSupportActionBar();
+        ColorDrawable color = new ColorDrawable(Color.parseColor("#FFE394"));
+        bar.setBackgroundDrawable(color);
+        bar.setTitle("Resources");
 
         // Initialise and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -49,5 +58,28 @@ public class ResourcesPage extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_top_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.notibutton:
+                //startActivity(new Intent(this, ));
+                overridePendingTransition(0, 0);
+                return true;
+
+            case R.id.profilebutton:
+                startActivity(new Intent(getApplicationContext(), ProfilePage.class));
+                overridePendingTransition(0, 0);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
