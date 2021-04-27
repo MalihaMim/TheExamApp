cc79f4b007309132647566ba94bcc36827cd3cdd
MalihaMim <69747651+MalihaMim@users.noreply.github.com>
2021-04-27 13:36:36 +1200

    Fitness page update 1

--- app/src/main/java/com/example/studynook/FitnessPage.java
+++ app/src/main/java/com/example/studynook/FitnessPage.java
@@ -3,8 +3,22 @@ package com.example.studynook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView;
 
-public class FitnessPage extends AppCompatActivity {
+public class FitnessPage<adapter> extends AppCompatActivity {
+
+    //get the spinner from the xml.
+    Spinner dropdown = findViewById(R.id.spinner);
+    //create a list of items for the spinner.
+    String[] items = new String[]{"Walking", "Running", "Cardio","Strength training","Dance","Sport"};
+    //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+    //There are multiple variations of this, but this is the basic variant.
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+    //set the spinners adapter to the previously created one.
+    adapter.setAdapter(adapter);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
