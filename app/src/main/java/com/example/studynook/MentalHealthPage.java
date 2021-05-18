08eb5c70b210cca8faf00ae9739488e85fc5567d
MalihaMim <69747651+MalihaMim@users.noreply.github.com>
2021-05-18 13:56:55 +1200

    Progress Bar on MentalHealthPage created

--- app/src/main/java/com/example/studynook/MentalHealthPage.java
+++ app/src/main/java/com/example/studynook/MentalHealthPage.java
@@ -8,12 +8,27 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MentalHealthPage extends AppCompatActivity {
+
+    private ProgressBar prog_bar;
+    private Button decrease;
+    private Button increase;
+    private TextView status;
+
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,33 @@ public class MentalHealthPage extends AppCompatActivity {
         bar.setDisplayHomeAsUpEnabled(true); // Displays the back button
         bar.setTitle("Mental Health");
 
+        prog_bar=(ProgressBar)findViewById(R.id.progress_bar);
+        decrease=(Button)findViewById(R.id.decrease_btn);
+        increase=(Button)findViewById(R.id.increase_btn);
+        status=(TextView)findViewById(R.id.percentage);
+
+        decrease.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable(){
+
+                    @Override
+                    public void run() {
+                        while(progressStatus<100){
+                            progressStatus+=1;
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    prog_bar.setProgress(progressStatus);
+                                    status.setText(progressStatus+"%");
+
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+        });
         // Initialise and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
