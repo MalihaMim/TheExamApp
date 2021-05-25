0f11fcd5dcb71ad5e9f2743d5edf692b43e4aa49
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2021-05-25 19:47:41 +1200

    notification error

--- app/src/main/java/com/example/studynook/EditCalendarEvent.java
+++ app/src/main/java/com/example/studynook/EditCalendarEvent.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.DatePickerDialog;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -19,6 +21,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
